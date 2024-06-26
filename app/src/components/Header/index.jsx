58703e03f66601335b2c1f76afd1962df709385b
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2024-06-26 20:19:17 -0300

    animação menu concluído

--- app/src/components/Header/index.jsx
+++ app/src/components/Header/index.jsx
@@ -1,13 +1,42 @@
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 function Header() {
+  
+  const [menu, setMenu] = useState(false)
+
+  console.log(menu)
+
   return (
-    <header className={styles.header}>
+    <header 
+      className={`
+        ${styles.header} 
+        ${menu ? styles.showMenu : styles.hideMenu}`
+      }
+    >
       <nav className={styles.headerItens}>
         <div className={styles.leftItens}>
-          <i class="fa-solid fa-bars"></i>
+          <i 
+            class="fa-solid fa-bars"
+            onClick={() => setMenu(!menu)}
+          ></i>
           <hr className={styles.line}/>
-          <span>Início</span>
+          <div 
+            className={styles.pageSections}
+          >
+            <span className={styles.mainSection}>Início</span>
+            <span 
+              className={styles.otherSections} 
+              style={{opacity: !menu ? "0" : "1", cursor: !menu ? "default" : "pointer"}}>
+              Sobre Mim
+            </span>
+            <span 
+              className={styles.otherSections} 
+              style={{opacity: !menu ? "0" : "1", cursor: !menu ? "default" : "pointer"}}>
+              Projetos
+            </span>
+          </div>
+          
         </div>
       </nav>
     </header>
