9e04872bf99e85a19515d92cc72278ffac775111
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2024-06-27 16:59:08 -0300

    header responsivo

--- app/src/components/Header/index.jsx
+++ app/src/components/Header/index.jsx
@@ -1,43 +1,81 @@
 import styles from "./Header.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Header() {
-  
+
   const [menu, setMenu] = useState(false)
 
-  console.log(menu)
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
-    <header 
+    <header
       className={`
         ${styles.header} 
         ${menu ? styles.showMenu : styles.hideMenu}`
       }
+      style={{
+        flexDirection: windowSize.width <= 460 && menu? "column" : "", 
+        height: windowSize.width <= 460 && menu? "225px" : ""
+      }}
     >
-      <nav className={styles.headerItens}>
-        <div className={styles.leftItens}>
-          <i 
-            class="fa-solid fa-bars"
-            onClick={() => setMenu(!menu)}
-          ></i>
-          <hr className={styles.line}/>
-          <div 
-            className={styles.pageSections}
-          >
-            <span className={styles.mainSection}>Início</span>
-            <span 
-              className={styles.otherSections} 
-              style={{opacity: !menu ? "0" : "1", cursor: !menu ? "default" : "pointer"}}>
-              Sobre Mim
-            </span>
-            <span 
-              className={styles.otherSections} 
-              style={{opacity: !menu ? "0" : "1", cursor: !menu ? "default" : "pointer"}}>
-              Projetos
-            </span>
-          </div>
-          
+      <nav 
+        className={styles.leftItens}
+        style={{
+          marginLeft: windowSize.width <= 460 && menu? "125px" : "",
+        }}
+      >
+        <i
+          class="fa-solid fa-bars"
+          onClick={() => setMenu(!menu)}
+        ></i>
+        <hr className={styles.line}/>
+        <div
+          className={styles.pageSections}
+        >
+          <span className={styles.mainSection}>Início</span>
+          <span
+            className={styles.otherSections}
+            style={{ opacity: !menu ? "0" : "1", cursor: !menu ? "default" : "pointer" }}>
+            Sobre Mim
+          </span>
+          <span
+            className={styles.otherSections}
+            style={{ opacity: !menu ? "0" : "1", cursor: !menu ? "default" : "pointer" }}>
+            Projetos
+          </span>
         </div>
+      </nav>
+      <nav
+        className={styles.rightItens}
+        style={{ 
+          marginTop: 
+          menu && windowSize.width > 460? "45px" : 
+          !menu && windowSize.width > 460? "0px": 
+          !menu && windowSize.width <= 460? "0px": 
+          menu && windowSize.width <= 460 && windowSize.height <= 650 ? "0px" : "0px", 
+          marginLeft: windowSize.width <= 460 && menu? "150px" : "",
+          marginBottom: menu && windowSize.width <= 460 && windowSize.height <= 650 ? "20px" : "20px", 
+        }}
+      >
+        <a href="https://github.com/marlonwillian" target="blank"><i class="fa-brands fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/marlon-willian-silva-barros-30b49122b/" target="blank"><i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://www.instagram.com/marlonw___/" target="blank"><i class="fa-brands fa-instagram"></i></a>
       </nav>
     </header>
   );
