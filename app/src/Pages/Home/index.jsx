51c632eb70762af70e7f5d59772f798a7c744459
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2024-07-01 22:28:44 -0300

    template do sobre mim concluído

--- app/src/Pages/Home/index.jsx
+++ app/src/Pages/Home/index.jsx
@@ -55,7 +55,92 @@ function Home() {
           </div>
         </div>
         <div className={styles.about}>
-          <span>Sobre Mim</span>
+          <div className={styles.aboutText}>
+            <span>Desde 2020, venho me dedicando ao estudo de programação, focado no desenvolvimento front-end, utilizando as tecnologias HTML, CSS, Java Script e React JS e outras com um conhecimento mais básico. Atualmente, sou estudante de Análise e Desenvolvimento de Sistemas, e no momento, sem experiência profissional.</span>
+          </div>
+          <div className={styles.aboutCourses}>
+            <h1>Cursos Extracurriculares</h1>
+            <hr/>
+            <div className={styles.course}>
+              <div className={styles.courseInfo}>
+                <h2>Nome do Curso</h2>
+                <h3>Empresa</h3>
+                <h4>tecnologia aprendida</h4>
+              </div>
+              <div className={styles.divButton}>
+                <button className={styles.certificateButton}>
+                  <span>Certificado</span> <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </button>
+              </div>
+            </div>
+            <div className={styles.course}>
+              <div className={styles.courseInfo}>
+                <h2>Nome do Curso</h2>
+                <h3>Empresa</h3>
+                <h4>tecnologia aprendida</h4>
+              </div>
+              <div className={styles.divButton}>
+                <button className={styles.certificateButton}>
+                  <span>Certificado</span> <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </button>
+              </div>
+            </div>
+            <div className={styles.course}>
+              <div className={styles.courseInfo}>
+                <h2>Nome do Curso</h2>
+                <h3>Empresa</h3>
+                <h4>tecnologia aprendida</h4>
+              </div>
+              <div className={styles.divButton}>
+                <button className={styles.certificateButton}>
+                  <span>Certificado</span> <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </button>
+              </div>
+            </div>
+            <div className={styles.course}>
+              <div className={styles.courseInfo}>
+                <h2>Nome do Curso</h2>
+                <h3>Empresa</h3>
+                <h4>tecnologia aprendida</h4>
+              </div>
+              <div className={styles.divButton}>
+                <button className={styles.certificateButton}>
+                  <span>Certificado</span> <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </button>
+              </div>
+            </div>
+            <div className={styles.course}>
+              <div className={styles.courseInfo}>
+                <h2>Nome do Curso</h2>
+                <h3>Empresa</h3>
+                <h4>tecnologia aprendida</h4>
+              </div>
+              <div className={styles.divButton}>
+                <button className={styles.certificateButton}>
+                  <span>Certificado</span> <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.allTecnologies}>
+            <hr/>
+            <div>
+              <h3>Principais</h3>
+              <i class="fa-brands fa-html5"></i>
+              <i class="fa-brands fa-css3"></i>
+              <i class="fa-brands fa-js"></i>
+              <i class="fa-brands fa-react"></i>
+              <h6>Conhecimento Básico</h6>
+              <i class="fa-brands fa-html5"></i>
+              <i class="fa-brands fa-css3"></i>
+              <i class="fa-brands fa-js"></i>
+              <i class="fa-brands fa-react"></i>
+              <i class="fa-brands fa-html5"></i>
+              <i class="fa-brands fa-css3"></i>
+              <i class="fa-brands fa-js"></i>
+              <i class="fa-brands fa-react"></i>
+            </div>
+          </div>
         </div>
         <div className={styles.projects}>
           <span>Projetos</span>
