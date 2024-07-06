ea1da11e80e11234c18e75effd503061b9587604
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2024-07-05 21:26:52 -0300

    card de projetos funcionando

---
+++ app/src/components/ProjectCard/index.jsx
@@ -0,0 +1,74 @@
+import styles from "./ProjectCard.module.css";
+import { useState } from "react";
+
+function ProjectCard({img, project, year, tecnologies, github, site, description}) {  
+  const [tab, setTab] = useState(1);
+
+  return (
+    <div className={styles.projectCard}>
+      <div className={styles.cardImg}>
+        <img src={img} alt="imagem do projeto" />
+      </div>
+      <div className={styles.cardInfo}>
+        <div className={styles.tabs}>
+          <span
+            className={`
+                  ${styles.tab1} 
+                  ${tab == 1 ? styles.selectedTab : ""}
+                  ${tab == 2 ? styles.tabAnimation : ""}
+                `}
+            onClick={() => setTab(1)}
+          >
+            Informações
+          </span>
+          <span
+            className={`
+                  ${styles.tab2} 
+                  ${tab == 2 ? styles.selectedTab : ""}
+                  ${tab == 1 ? styles.tabAnimation : ""}
+                `}
+            onClick={() => setTab(2)}
+          >
+            Descrição
+          </span>
+        </div>
+        <div
+          className={styles.informations}
+          style={{ display: tab == 1 ? "block" : "none" }}
+        >
+          <h1>{project}<span>{year}</span></h1>
+          <h2>
+            {
+              tecnologies.map((tecnology, index) => 
+                <span 
+                  className={styles.tecnology}
+                  style={{marginLeft: index == 0 ? "0px" : ""}}
+                >
+                  {tecnology}
+                </span>
+              )
+            }
+          </h2>
+          <a href={github} target="blank">
+            <span className={styles.github}>
+              <i class="fa-brands fa-square-github"></i> Ver o repositório
+            </span>
+          </a>
+          <a href={site} target="blank">
+            <span className={styles.site}>
+              <i class="fa-solid fa-arrow-up-right-from-square"></i> Ir para o site
+            </span>
+          </a>
+        </div>
+        <div
+          className={styles.description}
+          style={{ display: tab == 2 ? "block" : "none" }}
+        >
+          <span>{description}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
--- app/src/components/Projects/index.jsx
+++ app/src/components/Projects/index.jsx
@@ -1,65 +1,24 @@
-import { useState } from "react";
 import styles from "./Projects.module.css";
-import img from "./img/lojadejogos.png"
-
+import project from "../../json/projects.json";
+import ProjectCard from "../ProjectCard";
+ 
 function Projects() {
-  const [tab, setTab] = useState(1);
-
   return (
     <section className={styles.projects}>
       <div className={styles.divCards}>
-        <div className={styles.projectCard}>
-          <div className={styles.cardImg}>
-            <img src={img} alt="imagem do projeto"/>
-          </div>
-          <div className={styles.cardInfo}>
-            <div className={styles.tabs}>
-              <span 
-                className={`
-                  ${styles.tab1} 
-                  ${tab == 1 ? styles.selectedTab : ""}
-                  ${tab == 2 ? styles.tabAnimation : ""}
-                `}
-                onClick={() => setTab(1)}
-              >
-                Informações
-              </span>
-              <span 
-                className={`
-                  ${styles.tab2} 
-                  ${tab == 2 ? styles.selectedTab : ""}
-                  ${tab == 1 ? styles.tabAnimation : ""}
-                `}
-                onClick={() => setTab(2)}
-              >
-                Descrição
-              </span>
-            </div>
-            <div 
-              className={styles.informations}
-              style={{display: tab == 1 ? "block" : "none"}}
-            >
-              <h1>Nome do Projeto <span>2024</span></h1>
-              <h2>lista de tecnologias</h2>
-              <a href="">
-                <span className={styles.github}>
-                  <i class="fa-brands fa-square-github"></i> Ver no Github
-                </span>
-              </a>
-              <a href="">
-                <span className={styles.site}>
-                  <i class="fa-solid fa-arrow-up-right-from-square"></i> Ir para o site
-                </span>
-              </a>
-            </div>
-            <div 
-              className={styles.description}
-              style={{display: tab == 2 ? "block" : "none"}}
-            >
-              <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque labore, eveniet esse est alias quos vel modi consectetur commodi ipsum ea perspiciatis architecto qui veniam non rem corporis facilis explicabo.</span>
-            </div>
-          </div>
-        </div>
+        {
+          project.map((project) => 
+            <ProjectCard
+              img={project.img}
+              project={project.project}
+              year={project.year}
+              tecnologies={project.tecnologies}
+              github={project.github}
+              site={project.site}
+              description={project.description}
+            />
+          )
+        }
       </div>
     </section>
   );
