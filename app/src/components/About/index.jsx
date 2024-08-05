d0c230f4bdffad6cedf7f34215192665b884efa7
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2024-08-05 15:39:12 -0300

    About alterado

--- app/src/components/About/index.jsx
+++ app/src/components/About/index.jsx
@@ -4,16 +4,16 @@ function About() {
   return (
     <section className={styles.about} id="Sobre Mim">
       <div className={styles.aboutText}>
-        <span>Desde 2022, venho me dedicando ao estudo de programação, focado no desenvolvimento front-end, utilizando as tecnologias HTML, CSS, Java Script e React JS e outras com um conhecimento mais básico. Atualmente, sou estudante de Análise e Desenvolvimento de Sistemas, e no momento, sem experiência profissional.</span>
+        <span>Desde 2022, venho me dedicando ao estudo de programação,no desenvolvimento front-end, utilizando tecnologias comoHTML, CSS, JavaScript, React e NextJS com principais. Alémdisso, possuo conhecimentos básicos no Back-End, com NodeJS, SQL e MongoDB.</span>
         <div className={styles.allTecnologies}>
           <hr/>
-          <div>
+          <div className={styles.tecnologiesLogo}>
             <i class="fa-brands fa-html5"></i>
             <i class="fa-brands fa-css3"></i>
             <i class="fa-brands fa-js"></i>
             <i class="fa-brands fa-react"></i>
-            <i class="fa-brands fa-java"></i>
-            <i class="fa-brands fa-python"></i>
+            <i class="fa-brands fa-node-js"></i>
+            <div><i class="fa-solid fa-database"></i><span>SQL | MongoDB</span></div>
           </div>
         </div>
       </div>
